2326bc4e46154a327f9fe7a0f14d9e475bc02c73
deepakjanardhana <deepak_janardhana@yahoo.com>
2016-07-12 20:05:57 -0700

    Added class_config class

---
+++ class_config.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "strings.h"
+#include "class_config.h"
+
+using namespace std;
+
+
+class_config (){
+
+  cout<< "Welcome. System configuring..."
+  
+}
---
+++ class_config.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Strings.h"
+
+using namespace std;
+
+class class_config {
+
+ public: class_config();
+  
+}
